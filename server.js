b712e423e565135b1ee4c7740131ccd706aa4e0d
Luke Wood <lewoodwebdev@gmail.com>
2017-05-13 19:02:17 +0000

    added bing search api information

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 'use strict';
 var express = require('express');
 var path = require('path');
+var accountKey = 'npCUoIflwPpdgKb1AC67R9K9LpY8N8+beWni+8T85QE';
+var Bing = require('node-bing-api')({ accKey: accountKey });
+
 
 var app = express();
 
 
-app.get('/search/*', function(req, res, next) {
-  res.setHeader('content-type', 'plain/text');
-  // var type = typeof(res);
-  // var reqParams = req.params;
-  res.send('foo');
+app.get('/search/:terms', function(req, res, next) {
+  var terms = req.params;
+  res.send(terms);
 });
-app.get('/search', function(req,res){
-  res.send('search route');
+
+
+app.get('/search', function(req, res, next){
+  Bing.images("Ninja Turtles", {
+  top: 15,   // Number of results (max 50) 
+  skip: 3    // Skip first 3 result 
+  }, function(error, res, body){
+    console.log(body);
+  }), next();
+  res.send('search successful');
+  
 });
 
 
