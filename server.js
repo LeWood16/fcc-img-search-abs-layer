f4eb64376c8d9ab70890923bbb505b37e4d4d27f
Luke Wood <lewoodwebdev@gmail.com>
2017-05-13 14:52:17 +0000

    Express route rearranging; attempt to fix download bug

--- server.js
+++ server.js
@@ -1,33 +1,23 @@
 'use strict';
 var express = require('express');
 var path = require('path');
-var GoogleImages = require('google-images');
-
-const CSE_ID = '018394045321279107056:cppxvao4_ya';
-const CSE_API_KEY = 'AIzaSyBbWwyIkfHUmUpSqpaDUHs_xWK5c8NlSd4';
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+
+app.get('/search/*', function(req, res, next) {
+  res.setHeader('content-type', 'plain/text');
+  // var type = typeof(res);
+  // var reqParams = req.params;
+  res.send('foo');
 });
-
-
-
 app.get('/search', function(req,res){
   res.send('search route');
 });
 
-app.get('/search/*', function(req, res, next) {
-  // res.setHeader('content-type', 'plain/text');
-  var type = typeof(res);
-  var reqParams = req.params;
-  res.send(type == reqParams);
-});
 
-
-app.get('/latest', function(req,res){
-  res.status(200).json({ name: 'latest route' });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 var server = app.listen(process.env.PORT || 3000, function(){
